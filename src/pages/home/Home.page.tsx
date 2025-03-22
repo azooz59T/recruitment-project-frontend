689961c5462ed8f0ca5216c377b9a767b8cea8b0
azooz59T <mohamedoo1997@hotmail.com>
2025-03-22 16:42:11 +0000

    Create Home Page with lorem ipsum

---
+++ src/pages/home/Home.page.tsx
@@ -0,0 +1,18 @@
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className="content home">
+        <h3>Welcome To Website</h3>
+        <br />
+        <br />
+        <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Expedita est quam fuga cum omnis sapiente iure excepturi,
+            error asperiores, eum aperiam qui autem consectetur quisquam vel quis ipsa exercitationem ipsam!
+        </span>
+    </div>
+  )
+}
+
+export default Home
